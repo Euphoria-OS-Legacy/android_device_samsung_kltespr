0d4ae94ddd785c4ab23c02431f13a5515189cade
David Smit <dsmitty166@gmail.com>
2015-01-28 19:05:22 -0600

    Euphorify

---
+++ eos.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/kltespr/full_kltespr.mk)
+
+PRODUCT_DEVICE := kltespr
+PRODUCT_NAME := eos_kltespr
